6891fdb2cdcbcf14a471f93a6f20e4d7bb870e0d
aronei44 <meliodasmeliodas224@gmail.com>
2026-02-24 08:50:34 +0700

    refactor: app with middleware handler

--- src/app.py
+++ src/app.py
@@ -1,38 +1,48 @@
 from .request import Request
 from .response import Response
 from .router import Router
+from .route import Route
 
 class App:
     def __init__(self):
         self.router = Router()
+        self.middlewares = []
 
-    def route(self, method: str, path: str):
+    def route(self, method, path):
         def decorator(fn):
-            self.router.add(method, path, fn)
+            route = Route(method, path, fn)
+            self.router.add(route)
             return fn
         return decorator
+
+    async def handle_request(self, request):
+        handler, path_params = self.router.resolve(
+            request.method,
+            request.path
+        )
+
+        if not handler:
+            return Response(b"Not Found", status=404)
+
+        params = {}
+        params.update(path_params or {})
+        params.update(request.query_params)
+
+        result = handler(**params)
+        if hasattr(result, "__await__"):
+            result = await result
+
+        return result
 
     async def __call__(self, scope, receive, send):
         if scope["type"] != "http":
             return
 
         request = Request(scope, receive)
-        handler, path_params = self.router.match(
-            request.method,
-            request.path
-        )
 
-        if not handler:
-            response = Response(b"Not Found", status=404)
-        else:
-            params = {}
-            params.update(path_params or {})
-            params.update(request.query_params)
-            result = handler(**params)
+        handler = self.handle_request
+        for mw in reversed(self.middlewares):
+            handler = mw(handler)
 
-            if hasattr(result, "__await__"):
-                result = await result
-
-            response = result
-
+        response = await handler(request)
         await response.send(send)
--- src/router.py
+++ src/router.py
@@ -1,12 +1,11 @@
-from .route import Route
 class Router:
     def __init__(self):
         self.routes = []
 
-    def add(self, method, path, handler):
-        self.routes.append(Route(method, path, handler))
+    def add(self, route):
+        self.routes.append(route)
 
-    def match(self, method, path):
+    def resolve(self, method, path):
         for route in self.routes:
             params = route.match(method, path)
             if params is not None:
